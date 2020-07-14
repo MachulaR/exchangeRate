9ce4f617538d0050a2f3ba8c4f13c7d9597d6592
machula.remigiusz <machula.remigiusz@gmail.com>
2020-07-14 23:54:03 +0200

    display data from query with random vars

--- src/Controller/InquiryController.php
+++ src/Controller/InquiryController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
 
 class InquiryController extends AbstractController
 {
+    const NBP_API = 'http://api.nbp.pl/api/';
+    const MAX_DAYS_FOR_QUERY = 367;
+    const DATA_FORMAT_API = '/?format=json';
+    const FIRST_DATA_IN_API = '2002-01-02';
+
     public function __construct()
     {
     }
@@ -14,8 +20,21 @@ class InquiryController extends AbstractController
     /**
      * @Route("/", name="inquiry")
      */
-    public function mainPage(){
+    public function mainPage()
+    {
+        $client = new Client([
+            'base_uri' => self::NBP_API,
+        ]);
 
-        return $this->render('inquiry.html.twig');
+        $response = $client->request('GET', 'exchangerates/rates/c/USD/2020-01-02/2020-01-08/?format=json');
+        $responseAPI = json_decode($response->getBody()->getContents());
+//        dump($responseAPI);die;
+
+        $viewData = [
+            'data' => $responseAPI,
+        ];
+
+
+        return $this->render('inquiry.html.twig', $viewData);
     }
 }
