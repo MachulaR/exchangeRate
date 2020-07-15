b1a4cba34ed5b29b91802a96abb5067bedfc94f7
machula.remigiusz <machula.remigiusz@gmail.com>
2020-07-15 13:04:59 +0200

    setting startDate at first financial exchange day due to no data in nonfinancial exchange day

--- src/Repository/CurrencyInquiryRepository.php
+++ src/Repository/CurrencyInquiryRepository.php
@@ -6,6 +6,9 @@ use App\Entity\DTO\CurrencyInquiryDTO;
 use App\Exception\DateException;
 use DateTime;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 
 class CurrencyInquiryRepository
 {
@@ -20,39 +23,72 @@ class CurrencyInquiryRepository
 
     public function getDataFromApi(CurrencyInquiryDTO $currencyInquiryDTO)
     {
-        $startDate = $currencyInquiryDTO->getStartAt()->format('Y-m-d');
-        $endDate = $currencyInquiryDTO->getEndAt()->format('Y-m-d');
-        $currency = $currencyInquiryDTO->getCurrency();
+        $currencyInquiryDTO->setStartAt($currencyInquiryDTO->getStartAt()->format('Y-m-d'));
+        $currencyInquiryDTO->setEndAt($currencyInquiryDTO->getEndAt()->format('Y-m-d'));
 
         try
         {
-            $this->checkDate($startDate, $endDate);
+            $this->checkIfDatesAreCorrect($currencyInquiryDTO);
         }
         catch (DateException $dateException)
         {
             return null;
         }
 
+        $this->setStartDateAtFinancialExchangeDay($currencyInquiryDTO);
+
         $client = new Client([
             'base_uri' => self::NBP_API,
         ]);
 
-        $uri = 'exchangerates/rates/c/'.$currency.'/'.$startDate.'/'.$endDate.self::DATA_FORMAT_API;
+        $uri = 'exchangerates/rates/c/'.$currencyInquiryDTO->getCurrency().'/'.$currencyInquiryDTO->getStartAt().'/'.$currencyInquiryDTO->getEndAt().self::DATA_FORMAT_API;
         $response = $client->request('GET', $uri);
         $responseAPI = json_decode($response->getBody()->getContents());
 
         return $responseAPI;
     }
 
-    private function checkDate($startDate, $endDate)
+    private function setStartDateAtFinancialExchangeDay(CurrencyInquiryDTO $currencyInquiryDTO)
     {
-        if(strtotime($startDate)<strtotime(self::FIRST_DATA_IN_API)){
+        if (strtotime($currencyInquiryDTO->getStartAt()) != strtotime(self::FIRST_DATA_IN_API)) {
+
+            $client = new Client([
+                'base_uri' => self::NBP_API,
+            ]);
+
+            $break = false;
+            while ($break == false) {
+                $uri = 'exchangerates/rates/c/' . $currencyInquiryDTO->getCurrency() . '/' . $currencyInquiryDTO->getStartAt() . self::DATA_FORMAT_API;
+
+                try
+                {
+                    $response = $client->request('GET', $uri);
+                    $break = true;
+                }
+                catch (ClientException $exception)
+                {
+                    if ($exception->getCode() == 404)
+                    {
+                        $currencyInquiryDTO->setStartAt(date('Y-m-d', strtotime($currencyInquiryDTO->getStartAt() . ' -1day')));
+                    }
+                    else
+                    {
+                        //todo error occured (to handle)
+                    }
+                }
+            }
+        }
+    }
+
+    private function checkIfDatesAreCorrect(CurrencyInquiryDTO $currencyInquiryDTO)
+    {
+        if(strtotime($currencyInquiryDTO->getStartAt())<strtotime(self::FIRST_DATA_IN_API)){
             throw new DateException();
         }
 
         $today = new DateTime('NOW');
         $today = $today->format('Y-m-d');
-        if(strtotime($startDate)>strtotime($endDate) || strtotime($endDate)>strtotime($today)){
+        if(strtotime($currencyInquiryDTO->getStartAt())>strtotime($currencyInquiryDTO->getEndAt()) || strtotime($currencyInquiryDTO->getEndAt())>strtotime($today)){
             throw new DateException();
         }
     }
