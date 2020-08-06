a3d1248ac2293266a21bf9e24e858786867cfd85
machula.remigiusz <machula.remigiusz@gmail.com>
2020-08-06 16:35:27 +0200

    add more currency and default values for dates to form

--- src/Form/CurrencyInquiryType.php
+++ src/Form/CurrencyInquiryType.php
@@ -11,10 +11,26 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CurrencyInquiryType extends AbstractType
 {
+    const CURRENCY = [
+        'Dolar amerykański (USD)' => 'USD',
+        'Dolar australijski (AUD)' => 'AUD',
+        'Dolar kanadyjski (CAD)' => 'CAD',
+        'Euro (EUR)' => 'EUR',
+        'Forint (HUF)' => 'HUF',
+        'Frank szwajcarski (CHF)' => 'CHF',
+        'Funt szterling (GBP)' => 'GBP',
+        'jen (JPY)' => 'JPY',
+        'Korona czeska (CZK)' => 'CZK',
+        'Korona duńska (DKK)' => 'DKK',
+        'Korona norweska (NOK)' => 'NOK',
+        'Korona szwedzka (SEK)' => 'SEK',
+    ];
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('Start_at', DateType::class, [
+                'data' => new \DateTime(),
                 'attr' => [
                     'style' =>'margin:10px 25px 10px 4px'
                 ],
@@ -22,19 +38,18 @@ class CurrencyInquiryType extends AbstractType
                 'label' => 'Start at: ',
             ])
             ->add('End_at', DateType::class, [
+                'data' => new \DateTime(),
                 'attr' => [
                     'style' =>'margin:10px 25px 10px 4px'
                 ],
                 'widget' => 'single_text',
                 'label' => 'End at: ',
             ])
-            ->add('currency', ChoiceType::class, [
+            ->add('Currency', ChoiceType::class, [
                 'attr' => [
                     'style' =>'margin:10px 25px 10px 4px'
                 ],
-                'choices' => [
-                    'USD' => 'USD',
-                ],
+                'choices' => self::CURRENCY,
                 'label' => 'Currency:',
             ])
             ->add('Check', SubmitType::class)
